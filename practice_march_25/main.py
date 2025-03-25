debdb5001bef4ccfad59b55ec5d035d869fd239e
Bohdan Makohin <ajukraine@gmail.com>
2025-03-25 12:20:50 +0200

    feat: example of Open/Close principle

--- practice_march_25/main.py
+++ practice_march_25/main.py
@@ -58,6 +58,27 @@ class Account(ABC):
     return self._number
 
 
+class InterestTransaction(Transaction):
+  def __init__(self, account):
+    super().__init__(account, "INTEREST")
+
+  def execute(self, amount):
+    self._account.deposit(amount)
+
+
+class DepositAccount(Account):
+  def __init__(self, accountNumber, interestRate):
+    super().__init__(accountNumber)
+    self._interestRate = interestRate
+
+  def deposit(self, amount):
+    interestAmount = amount * self._interestRate
+    super().deposit(amount + interestAmount)
+
+  def _validate(self, amount):
+    pass
+
+
 class CreditAccount(Account):
   def _validate(self, amount):
     pass
@@ -71,25 +92,31 @@ class SavingsAccount(Account):
 
 # Main code
 
-creditAccount = CreditAccount("0001")
-savingsAccount = SavingsAccount("0002")
+# creditAccount = CreditAccount("0001")
+# savingsAccount = SavingsAccount("0002")
+depositAccount = DepositAccount("0003", 0.15)
 
-account = savingsAccount
+interestTransaction = InterestTransaction(depositAccount)
+interestTransaction.execute(100)
 
-salary = SalaryTransaction(account)
-payment = PaymentTransaction(account)
+print(f"Balance: {depositAccount.balance()}")
 
-transactions = [(salary, 100), (payment, 120)]
-
-for tx, amount in transactions:
-  # amount = random.randint(1, 100)
-  print(f"Amount: {amount}")
-  try:
-    tx.execute(amount)
-  except AccountingException as e:
-    print(f"Cannot execute transaction. Reason: {e}")
-  except Exception as e:
-    print(f"Generic error: {e}")
-  finally:
-    print(tx)
-    print(f"Account1 balance: {account.balance()}")
+# account = savingsAccount
+#
+# salary = SalaryTransaction(account)
+# payment = PaymentTransaction(account)
+#
+# transactions = [(salary, 100), (payment, 120)]
+#
+# for tx, amount in transactions:
+#   # amount = random.randint(1, 100)
+#   print(f"Amount: {amount}")
+#   try:
+#     tx.execute(amount)
+#   except AccountingException as e:
+#     print(f"Cannot execute transaction. Reason: {e}")
+#   except Exception as e:
+#     print(f"Generic error: {e}")
+#   finally:
+#     print(tx)
+#     print(f"Account1 balance: {account.balance()}")
